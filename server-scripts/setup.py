fe5205870baf2645083bc8dddbfbbea61db66458
Rohde Fischer <rohdef@rohdef.dk>
2022-07-10 12:17:04 +0200

    Added manual installation of ldap

--- server-scripts/setup.py
+++ server-scripts/setup.py
@@ -15,13 +15,22 @@ class ProvisionedServer(System):
 
         self._logger = logging.getLogger(__name__)
 
+
     def setup(self):
         self._logger.info("Setting up server")
         self._setDebConfSelections(self._autoInstall.debConfSelections().debConfSelections())
+        self._installPackages(["slapd", "ldap-utils"])
+
 
     def _setDebConfSelections(self, selections):
         echo = self._runPipe(["echo", "-e", selections])
         self._endPipe(["debconf-set-selections"], echo)
+
+
+    def _installPackages(self, packages):
+        self._logger.info(f"Installing packages: {packages}")
+        installCommand = ["apt-get", "install", "-y"] + packages
+        self._run(installCommand, False)
 
 
 if __name__ == "__main__":
