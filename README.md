65dc457c1e5c97b39feb003d41af998e645b8724
Rohde Fischer <rohdef@rohdef.dk>
2022-07-28 12:25:22 +0200

    Testing the debug plugin #4 #11

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# ServerSetup
+# Gourmet
 
-## Prerequisites
+Warning: This is a personal tool - feel free to use, etc. but there's no guarantees for your use case :)
+
+Tool for automating install tasks for machines in a declarative manner.
+Intended for servers, local machines etc., where tools like Teraform, helm and K8s Operators aren't useful.
+Format is inspired by GitHub Actions to keep it easy to get started with
+
+## Prerequisites for usage
 
 - `sudo apt install sshpass`
+
 
 ## Running
 
---
+++ src/nativeTest/kotlin/plugins/DebugTest.kt
@@ -0,0 +1,100 @@
+package plugins
+
+import configuration.Parameters
+import configuration.plugins.Debug
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import it.czerwinski.kotlin.util.Right
+import mu.Appender
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class DebugTest {
+    private val appender = CacheAppender()
+
+    @BeforeTest
+    fun `set appender`() {
+        mu.KotlinLoggingConfiguration
+            .appender = appender
+    }
+
+    private val parameters = Parameters.Map(
+        "text" to Parameters.String("some text"),
+        "boolean" to Parameters.Boolean(true),
+        "integer" to Parameters.Integer(30),
+        "list" to Parameters.List(
+            Parameters.String("list item"),
+            Parameters.Boolean(false),
+            Parameters.Integer(4),
+        ),
+        "map" to Parameters.Map(
+            "another text" to Parameters.String("map item"),
+            "another boolean" to Parameters.Boolean(true),
+            "another integer" to Parameters.Integer(9),
+        ),
+    )
+
+    @Test
+    fun `Doesn't update map`() {
+        val result = Debug.run(parameters)
+
+        result.shouldBe(Right(emptyMap()))
+    }
+
+    @Test
+    fun `Outputs parameters`() {
+        Debug.run(parameters)
+
+        val log = appender.messages.joinToString("\n")
+
+        checkContents(log, parameters)
+    }
+
+    private fun checkContents(log: String, parameters: Parameters) {
+        when (parameters) {
+            is Parameters.Boolean -> log.shouldContain(parameters.value.toString())
+            is Parameters.Integer -> log.shouldContain(parameters.value.toString())
+            is Parameters.List ->
+                parameters.value.forAll {
+                    checkContents(log, it)
+                }
+            is Parameters.Map -> {
+                parameters.value.forAll {
+                    log.shouldContain(it.key)
+                    checkContents(log, it.value)
+                }
+            }
+            is Parameters.String -> log.shouldContain(parameters.value)
+        }
+    }
+}
+
+private class CacheAppender : Appender {
+    private val _messages = mutableListOf<String>()
+    val messages: List<String>
+        get() = _messages.toList()
+
+    override val includePrefix: Boolean
+        get() = false
+
+    override fun debug(loggerName: String, message: String) {
+        _messages.add(message)
+    }
+
+    override fun error(loggerName: String, message: String) {
+        _messages.add(message)
+    }
+
+    override fun info(loggerName: String, message: String) {
+        _messages.add(message)
+    }
+
+    override fun trace(loggerName: String, message: String) {
+        _messages.add(message)
+    }
+
+    override fun warn(loggerName: String, message: String) {
+        _messages.add(message)
+    }
+}
