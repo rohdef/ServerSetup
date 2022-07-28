241d1a707d5db73513f61de5a5f7514fb8466aa3
Rohde Fischer <rohdef@rohdef.dk>
2022-07-28 12:26:02 +0200

    Debugging is now possible, closes #11

--- src/nativeMain/kotlin/configuration/plugins/Debug.kt
+++ src/nativeMain/kotlin/configuration/plugins/Debug.kt
@@ -4,6 +4,7 @@ import configuration.Parameters
 import configuration.engine.EngineError
 import configuration.engine.EnvironmentUpdates
 import it.czerwinski.kotlin.util.Either
+import it.czerwinski.kotlin.util.Right
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
@@ -12,6 +13,38 @@ object Debug : StepAction {
     override fun run(
         parameters: Parameters.Map,
     ): Either<EngineError, EnvironmentUpdates> {
-        TODO("not implemented")
+        logger.info { "Debug action is run with:" }
+
+        logger.info { formatParameters(parameters) }
+
+        return Right(emptyMap())
+    }
+
+    private fun formatParameters(parameters: Parameters, indentation: Int = 0): String {
+        val indent = "  ".repeat(indentation)
+
+        return when (parameters) {
+            is Parameters.Boolean -> parameters.value.toString()
+            is Parameters.Integer -> parameters.value.toString()
+            is Parameters.List -> {
+                val formatting = parameters.value
+                    .map { formatParameters(it, indentation + 1) }
+                    .map { "${indent}${it}" }
+                    .joinToString(",\n")
+
+                return formatting
+            }
+
+            is Parameters.Map -> {
+                val formatting = parameters.value
+                    .mapValues { formatParameters(it.value, indentation + 1) }
+                    .map { "${indent}${it.key}: ${it.value}" }
+                    .joinToString(",\n")
+
+                return "\n$formatting"
+            }
+
+            is Parameters.String -> parameters.value
+        }
     }
 }
