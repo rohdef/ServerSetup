5139c7892abd5c9536db65c598000a26604430f6
Rohde Fischer <rohdef@rohdef.dk>
2022-07-29 14:02:07 +0200

    Plugin for updating environment, closes #15

--- src/nativeMain/kotlin/configuration/plugins/UpdateEnvironment.kt
+++ src/nativeMain/kotlin/configuration/plugins/UpdateEnvironment.kt
@@ -4,11 +4,32 @@ import configuration.Parameters
 import configuration.engine.EngineError
 import configuration.engine.EnvironmentUpdates
 import it.czerwinski.kotlin.util.Either
+import it.czerwinski.kotlin.util.Left
+import it.czerwinski.kotlin.util.Right
+import it.czerwinski.kotlin.util.flatMap
 
 object UpdateEnvironment : StepAction {
     override fun run(
         parameters: Parameters.Map,
-    ): Either<EngineError, EnvironmentUpdates> {
-        TODO("not implemented")
+    ): Either<UpdateEnvironmentError, EnvironmentUpdates> {
+        return parameters.value.asSequence()
+            .fold(Right(emptyMap<String, String>()) as Either<UpdateEnvironmentError, EnvironmentUpdates>) { acc, entry ->
+                acc.flatMap {
+                    val parameter = entry.value
+                    when (parameter) {
+                        is Parameters.Boolean -> Right(it + (entry.key to parameter.value.toString()))
+                        is Parameters.Integer -> Right(it + (entry.key to parameter.value.toString()))
+                        is Parameters.List -> Left(UpdateEnvironmentError.ListNotAllowed(entry.key))
+                        is Parameters.Map -> Left(UpdateEnvironmentError.MapNotAllowed(entry.key))
+                        is Parameters.String -> Right(it + (entry.key to parameter.value))
+                    }
+                }
+            }
     }
+}
+
+sealed interface UpdateEnvironmentError : EngineError {
+    data class MapNotAllowed(val value: String) : UpdateEnvironmentError
+
+    data class ListNotAllowed(val value: String) : UpdateEnvironmentError
 }
---
+++ src/nativeTest/kotlin/plugins/UpdateEnvironmentTest.kt
@@ -0,0 +1,61 @@
+package plugins
+
+import configuration.Parameters
+import configuration.plugins.UpdateEnvironment
+import configuration.plugins.UpdateEnvironmentError
+import io.kotest.matchers.shouldBe
+import it.czerwinski.kotlin.util.Left
+import it.czerwinski.kotlin.util.Right
+import kotlin.test.Test
+
+class UpdateEnvironmentTest {
+    @Test
+    fun `Sends step parameters as environment updates`() {
+        val parameters = Parameters.Map(
+            "something" to Parameters.String("else"),
+            "enable" to Parameters.Boolean(true),
+            "issues" to Parameters.Integer(37),
+        )
+
+        val result = UpdateEnvironment.run(parameters)
+
+        val expected = Right(
+            mapOf(
+                "something" to "else",
+                "enable" to "true",
+                "issues" to "37",
+            )
+        )
+        result.shouldBe(expected)
+    }
+
+    @Test
+    fun `Does not allow maps`() {
+        val parameters = Parameters.Map(
+            "parameters" to Parameters.Map(
+                "age" to Parameters.String("37"),
+                "person" to Parameters.String("Bastard"),
+            ),
+        )
+
+        val result = UpdateEnvironment.run(parameters)
+
+        val expected = Left(UpdateEnvironmentError.MapNotAllowed("parameters"))
+        result.shouldBe(expected)
+    }
+
+    @Test
+    fun `Does not allow lists`() {
+        val parameters = Parameters.Map(
+            "something" to Parameters.List(
+                Parameters.String("Postman"),
+                Parameters.String("Milkman"),
+            ),
+        )
+
+        val result = UpdateEnvironment.run(parameters)
+
+        val expected = Left(UpdateEnvironmentError.ListNotAllowed("something"))
+        result.shouldBe(expected)
+    }
+}
